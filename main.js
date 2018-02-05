75dddc8c70d845062eb7b0640f1b38cb64505511
Pilar Chavez <pchavez92@gmail.com>
2018-02-05 11:51:52 -0800

    Included Bulma css with other things

--- main.js
+++ main.js
@@ -23,6 +23,26 @@ Vue.component('task-list', {
 Vue.component('task', {
   template: '<li><slot></slot></li>'
 });
+Vue.component('bulma-message', {
+  props: ['title', 'body'],
+  data () {
+    return {
+      isVisible: true
+    }
+  },
+  template: `
+  <article class="message" v-show="isVisible">
+    <div class="message-header">
+      <p>{{title}}</p>
+      <button class="delete" aria-label="delete" @click="isVisible = false"></button>
+    </div>
+    <div class="message-body">
+      {{body}}
+    </div>
+  </article>
+
+  `
+})
 new Vue({
   el:'#root'
 })
