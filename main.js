45e1bf3d07b7e5aac97c2f5aa9ee15e607f125a5
Pilar Chavez <pchavez92@gmail.com>
2018-02-10 14:15:40 -0800

    New modal incorporation hella cool

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ Vue.component('task-list', {
             </div>`,
   data () {
     return {
-
       tasks: [
         {task: 'Go to the store', complete: true},
 
@@ -20,29 +19,28 @@ Vue.component('task-list', {
     }
   }
 });
-Vue.component('task', {
-  template: '<li><slot></slot></li>'
-});
-Vue.component('bulma-message', {
-  props: ['title', 'body'],
-  data () {
-    return {
-      isVisible: true
-    }
-  },
+Vue.component('modal', {
   template: `
-  <article class="message" v-show="isVisible">
-    <div class="message-header">
-      <p>{{title}}</p>
-      <button class="delete" aria-label="delete" @click="isVisible = false"></button>
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div class="box">
+        <slot></slot>
+      </div>
     </div>
-    <div class="message-body">
-      {{body}}
-    </div>
-  </article>
-
-  `
+    <button class="modal-close is-large" @click="$emit('close')" aria-label="close"></button>
+  </div>
+  `,
+  methods: {
+    showModal() {
+      console.log('hello')
+    }
+  }
 })
+
 new Vue({
-  el:'#root'
+  el:'#root',
+  data: {
+    showModal: false
+  }
 })
